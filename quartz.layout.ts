bdf168b25c934aa39393174866a2bc24a2a0e232
Barun <admin@One2Ns-MacBook-Air.local>
2024-12-07 02:09:27 +0530

    layout changed

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [Component.Darkmode(), Component.Search()],
   afterBody: [],
   footer: Component.Footer({
     links: {
@@ -29,8 +29,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Darkmode(),
-    Component.Search(),
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
