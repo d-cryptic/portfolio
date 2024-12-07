aa4b569c7b18d96c99521a0d30028273df97212f
Barun <admin@One2Ns-MacBook-Air.local>
2024-12-07 14:56:19 +0530

    footer link to home page added

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          <a href="https://barundebnath.com">{i18n(cfg.locale).components.footer.createdWith} </a>{" "}
+          <a href="https://barundebnath.com">{🕹 i18n(cfg.locale).components.footer.createdWith} </a>{" "}
           © {year}
           <p>Follow me at: </p>
         </p>
