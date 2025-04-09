20fe63321e03e1ca976f5e4f53dc637699adf076
Barun <admin@One2Ns-MacBook-Air.local>
2025-04-09 22:04:01 +0530

    changed font

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Recursive", "system-ui", "sans-serif"],
+        body: ["Recursive", "system-ui", "sans-serif"],
+        mono: ["IBM Plex Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
